43a498b977e546f997dc51e652bc5e61dba50cde
americanman <snowboardracer7@gmail.com>
2019-05-05 20:54:51 -0400

    Add comments

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const flattenArray = (arr) => {
   let flat = [].concat(...arr);
+  // here I would call flattanArray function recursively
+  // other words calling itself if some-method returns true
   return flat.some(Array.isArray) ? flattenArray(flat) : flat;
 }
 
